6698fe6607ccb46241214ffbf9a22a822090f011
jdittert <jondittert@gmail.com>
2023-03-03 12:54:48 -0500

    Update addBook function so it works

--- script.js
+++ script.js
@@ -17,31 +17,24 @@ function addBookToLibrary(item) {
 addBookToLibrary(bookOne);
 addBookToLibrary(bookTwo);
 
-function addBook(title) {
+function addBook(Book) {
     const table = document.getElementById("book-table");
     const row = table.insertRow(-1);
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
     const cell3 = row.insertCell(2);
     const cell4 = row.insertCell(3);
-    cell1.innerText = title;
-    cell2.innerText = "Blarg";
-    cell3.innerText = "Cheese";
-    cell4.innerText = "Whiz";
+    cell1.innerText = Book.title;
+    cell2.innerText = Book.author;
+    cell3.innerText = Book.pages;
+    cell4.innerText = Book.status;
 }
 
-// I thought this would work, but it doesn't //
 function populateTable() {    
     myLibrary.forEach(addBook);    
 }
 
-// This also does not work //
-function populateRow() {
-    const {title} = this.title;
-    addBook(title);
-}
-
 const populate = document.getElementById("populate");
-populate.addEventListener("click", populateRow);
+populate.addEventListener("click", populateTable);
 
 console.log(myLibrary);
